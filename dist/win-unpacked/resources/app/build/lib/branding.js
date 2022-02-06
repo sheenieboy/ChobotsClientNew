80daacb60f052d831101cedc52db72ad95e36643
sheenieboy <sheenieboy@users.noreply.github.com>
2022-02-06 18:00:34 +0000

    Delete dist directory

--- dist/win-unpacked/resources/app/build/lib/branding.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/*
-    xyz.puyo.club, xyz.puyo.club.chotopia
-    Copyright (C) 2022, Puyo <hi@puyo.xyz>, all rights reserved.
-*/
-Object.defineProperty(exports, "__esModule", { value: true });
